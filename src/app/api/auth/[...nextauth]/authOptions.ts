0383aeb0183ba08c213046c8e90a31bc93573c58
dragox134 <legi1810@gmail.com>
2024-11-25 11:55:31 +0100

    init

---
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,27 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import { prisma } from "./prizma";
+import GoogleProvider from "next-auth/providers/google";
+
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: true, // Enable/disable detailed logging
+  pages: {
+    signIn: '/auth/prihlasenie',
+    signOut: '/auth/odhlasenie',
+  },
+  callbacks: {
+    async redirect({ url, baseUrl }: { url: string; baseUrl: string }) {
+      // Redirect to home page after sign-in
+      return baseUrl || url; // baseUrl is automatically set from NEXTAUTH_URL in .env
+    },
+  },
+};
---
+++ src/app/auth/prihlasenie/page.tsx
@@ -0,0 +1,9 @@
+// src/app/auth/prihlasenie/page.tsx
+
+import SignInView from "../../../sections/SigInView";
+
+export const metadata = { title: "Prihlásenie užívateľa | ZoškaSnap" };
+
+export default function SignInPage() {
+  return <SignInView />;
+}
