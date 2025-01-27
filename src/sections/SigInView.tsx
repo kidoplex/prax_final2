9961e858a5fe2d2d0beed431897e28370ca5f78a
dragox134 <legi1810@gmail.com>
2025-01-27 12:08:32 +0100

    removed unnecesery checkbox in log in

--- src/sections/SigInView.tsx
+++ src/sections/SigInView.tsx
@@ -4,27 +4,15 @@ import {
   Button,
   Container,
   Typography,
-  Checkbox,
-  FormControlLabel,
-  Box,
 } from "@mui/material";
 import { signIn } from "next-auth/react";
 import GoogleIcon from "@mui/icons-material/Google";
 import Link from "next/link";
-import { useState } from "react";
 
 export default function SignInView() {
-  const [gdprChecked, setGdprChecked] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
   
 
   const handleSignIn = () => {
-    if (!gdprChecked) {
-      setShowAlert(true); // Show alert if GDPR is not checked
-      return;
-    }
-
-    setShowAlert(false); // Hide alert if GDPR is checked
     const baseUrl = window.location.origin;
     signIn("google", {callbackUrl: `${baseUrl}/prispevok`,});
   };
@@ -55,47 +43,6 @@ export default function SignInView() {
           Zaregistrujte sa
         </Link>
       </Typography>
-
-
-      {/* GDPR Consent */}
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={gdprChecked}
-            onChange={(e) => setGdprChecked(e.target.checked)}
-            color="primary"
-          />
-        }
-        label={
-          <Typography>
-            Súhlas s{" "}
-            <Link href="/gdpr" style={{ color: "blue", textDecoration: "underline" }}>
-              GDPR
-            </Link>
-          </Typography>
-        }
-        sx={{ mb: 2 }}
-      />
-
-      {/* Conditional Alert */}
-      {showAlert && (
-        <Box
-          sx={{
-            bgcolor: "#fdecea",
-            color: "#d32f2f",
-            border: "1px solid #d32f2f",
-            borderRadius: 1,
-            p: 2,
-            textAlign: "center",
-            mb: 2,
-            width: "100%",
-          }}
-        >
-          <Typography>
-            Prosím, súhlaste s podmienkami GDPR pred pokračovaním.
-          </Typography>
-        </Box>
-      )}
 
       {/* Google Sign In */}
       <Button
